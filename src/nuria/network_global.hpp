52828835b4b98471a759fbbe66f8d65231272f1d
Alexander Siol <dunklermeuchler@gmail.com>
2014-07-28 12:17:15 +0200

    Changed NURIA_NETWORK_LIBRARY to cmake-defined NuriaNetwork_EXPORTS
    This provides an all-around cleaner solution, as NURIA_NETWORK_LIBRARY
    would be set in tests, too.

--- src/nuria/network_global.hpp
+++ src/nuria/network_global.hpp
@@ -20,7 +20,7 @@
 
 #include <QtCore/qglobal.h>
 
-#if defined(NURIA_NETWORK_LIBRARY)
+#if defined(NuriaNetwork_EXPORTS)
 #  define NURIA_NETWORK_EXPORT Q_DECL_EXPORT
 #else
 #  define NURIA_NETWORK_EXPORT Q_DECL_IMPORT
